8b55db385d129cc2bfddf2f0c85bc094c818928b
Velmurugan Gugan <velmurugan@heartlandstg.org>
2021-07-25 18:58:47 +0530

    changes added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,7 @@ const Sidebar = () => {
     return <>
         <Card className='p-0-content w-100'>
             <div className="p-grid m-0 align-middle" style={{ background: '#ffffff' }}>
-            <img className='mb-2' style={{ width: 64, height: 64, marginTop: -16 }} src={`${process.env.PUBLIC_URL}/LOGO.svg`} alt={'Logo'} />
+            <img className='mb-2' style={{ width: 64, height: 64, marginTop: -16 }} src={`${process.env.PUBLIC_URL}/logo.svg`} alt={'Logo'} />
                 {
                     icons.map((c, i) => <div key={i} className={`mb-2`}>
                         <img style={{ width: 20, height: 20 }} src={`${process.env.PUBLIC_URL}/images/${c}.svg`} alt={c} />
